56ac4c21ad32e4dc37253ddc0469a27c964b25e7
KurumiWWW <2209278137@qq.com>
2023-05-24 23:20:28 +0800

    rider

--- 001-开始/Program.cs
+++ 001-开始/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System; // 引入命名空间
 
-/**
- * C#编译 
- * 代码->程序集(exe dll)
- * 程序集在运行时会编译成机器指令(JIT即时编译)
+/*
+  C#编译 
+  代码->程序集(exe dll)
+  程序集在运行时会编译成机器指令(JIT即时编译)
  */
-namespace _001_开始 {
-  class Program {
-    static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
-
+namespace _001_开始
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
-  }
 }
 
-namespace new_name {
-
+namespace new_name
+{
 }
--- 003-变量/Program.cs
+++ 003-变量/Program.cs
@@ -1,43 +1,50 @@
 ﻿using System.Collections.Specialized;
 
-namespace _003 {
-  class Program {
-    static void Main(string[] args) {
-      Demo d = new Demo();
+namespace _003_变量
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
 
-      d.formatStr();
-    }
-  }
-
-  class Demo {
-    public void printFun() {
-      //char a = 'c';
-      //int b = a;
-      //Console.WriteLine(b); // 99 ascii
-      int a = 97;
-      //char b = a; // 错误 整数类型过大，无法转换为char类型
-      char b = (char)a; // 强转 如果大于目标类型 多出部分会丢失
-      Console.WriteLine(b);
+            d.FormatStr();
+        }
     }
 
-    public void inputFun() {
-      Console.WriteLine(@"c:\\a 
+    class Demo
+    {
+        public void PrintFun()
+        {
+            //char a = 'c';
+            //int b = a;
+            //Console.WriteLine(b); // 99 ascii
+            int a = 97;
+            //char b = a; // 错误 整数类型过大，无法转换为char类型
+            char b = (char)a; // 强转 如果大于目标类型 多出部分会丢失
+            Console.WriteLine(b);
+        }
+
+        public void InputFun()
+        {
+            Console.WriteLine(@"c:\\a 
 
       ""
       \b\c");
-      // 引号前加@符 引号內部转义符可变为普通字符 支持直接换行，连续两个双引号输出为引号
+            // 引号前加@符 引号內部转义符可变为普通字符 支持直接换行，连续两个双引号输出为引号
 
-      //string str = Console.ReadLine(); // 从控制台读取输入一行内容 返回字符串
-      //Console.WriteLine(str);
+            //string str = Console.ReadLine(); // 从控制台读取输入一行内容 返回字符串
+            //Console.WriteLine(str);
 
-      int strInt = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("用户输入了：" + strInt);
+            int strInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("用户输入了：" + strInt);
+        }
+
+        public void FormatStr()
+        {
+            // 字符串格式化输出
+            int a = 1, b = 2;
+            Console.WriteLine("格式化输出：{0}+{1}={2}", a, b, a + b);
+        }
     }
-
-    public void formatStr() {
-      // 字符串格式化输出
-      int a = 1, b = 2;
-      Console.WriteLine("格式化输出：{0}+{1}={2}", a, b, a + b);
-    }
-  }
 }
